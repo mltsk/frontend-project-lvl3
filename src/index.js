8abac85bd9df30c59c5e082bebec009dc251ec72
Dmitrii Maletskii <mltsk94@gmail.com>
2021-09-05 13:37:43 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -4,38 +4,40 @@ import validate from './validator.js';
 import initView from './view.js';
 import timer from './timer.js';
 
-const state = {
-  urls: [],
-  form: {
-    status: 'filling',
-    input: {
-      error: null,
+document.addEventListener('DOMContentLoaded', () => {
+  const state = {
+    urls: [],
+    form: {
+      status: 'filling',
+      input: {
+        error: null,
+      },
     },
-  },
-  feeds: [],
-  posts: [],
-};
+    feeds: [],
+    posts: [],
+  };
 
-const elements = {
-  form: document.querySelector('.rss-form'),
-  input: document.querySelector('.form-control'),
-  feedback: document.querySelector('.feedback'),
-  posts: document.querySelector('.posts'),
-  feeds: document.querySelector('.feeds'),
-  button: document.querySelector("[aria-label='add']"),
-  previewBtn: document.querySelectorAll("[data-bs-target='#modal']"),
-  modalTitle: document.querySelector('.modal-title'),
-  modalBody: document.querySelector('.modal-body'),
-};
+  const elements = {
+    form: document.querySelector('.rss-form'),
+    input: document.querySelector('.form-control'),
+    feedback: document.querySelector('.feedback'),
+    posts: document.querySelector('.posts'),
+    feeds: document.querySelector('.feeds'),
+    button: document.querySelector("[aria-label='add']"),
+    previewBtn: document.querySelectorAll("[data-bs-target='#modal']"),
+    modalTitle: document.querySelector('.modal-title'),
+    modalBody: document.querySelector('.modal-body'),
+  };
 
-const watched = initView(state, elements);
+  const watched = initView(state, elements);
 
-timer(watched);
-console.log(elements.form);
-document.querySelector('.rss-form').addEventListener('submit', (event) => {
-  event.preventDefault();
-  watched.form.input.error = null;
-  const formData = new FormData(event.target);
-  const url = formData.get('url');
-  validate(url, watched);
+  timer(watched);
+  console.log(elements.form);
+  document.querySelector('.rss-form').addEventListener('submit', (event) => {
+    event.preventDefault();
+    watched.form.input.error = null;
+    const formData = new FormData(event.target);
+    const url = formData.get('url');
+    validate(url, watched);
+  });
 });
