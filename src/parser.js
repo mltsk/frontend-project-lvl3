553aa4beeda83e42238fd96351646772348b2ae2
Dmitrii Maletskii <mltsk94@gmail.com>
2021-09-05 16:52:41 +0300

    refactioring parser

--- src/parser.js
+++ src/parser.js
@@ -1,11 +1,10 @@
 function parse(contents) {
   const parser = new DOMParser();
-  const parsererrorNS = parser.parseFromString('INVALID', 'application/xml').getElementsByTagName('parsererror')[0].namespaceURI;
-  const dom = parser.parseFromString(contents, 'application/xml');
-  if (dom.getElementsByTagNameNS(parsererrorNS, 'parsererror').length > 0) {
+  const data = parser.parseFromString(contents, 'application/xml');
+  if (data.getElementsByTagName('parsererror').length > 0) {
     throw new Error('Error parsing XML');
   }
-  return dom.children[0].children[0].children;
+  return data.children[0].children[0].children;
 }
 
 export default parse;
